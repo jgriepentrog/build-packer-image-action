0d9fd044c26fa86691a8e0fbff93e1640b52dfdb
John Griepentrog <1390583+jgriepentrog@users.noreply.github.com>
2020-05-24 11:08:06 -0500

    Try a JS action

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { execSync } = require("child_process");
+
+try {
+  console.log(execSync('sudo apt-get -y install build-essential virtualbox-6.1'))
+  
+  const template = core.getInput('template');
+  console.log(`Building template ${template}...`);
+
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}

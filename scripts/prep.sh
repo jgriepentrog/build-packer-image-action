eb5955ab759b6126ab691218bfd0bd34f90598fd
John Griepentrog <1390583+jgriepentrog@users.noreply.github.com>
2020-05-26 22:58:19 -0500

    Switch to Javascript Action (#1)
    Switch to using a Javacript Action

---
+++ scripts/prep.sh
@@ -0,0 +1,8 @@
+export DIST=bionic
+export ORACLE_VBOX_KEY=oracle_vbox_2016.asc
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian ${DIST} contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+wget "https://www.virtualbox.org/download/${ORACLE_VBOX_KEY}"
+sudo apt-key add $ORACLE_VBOX_KEY
+rm -f $ORACLE_VBOX_KEY
+sudo apt-get -y update
+sudo apt-get -y install virtualbox-6.1
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require("child_process");
+
+const execCmd = async (cmd) => {
+  const runningCmd = exec(cmd);
+
+  runningCmd.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  runningCmd.stderr.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  return new Promise((resolve, reject) => {
+    runningCmd.on('err', (err) => {
+      console.log(err);
+    })
+
+    runningCmd.on('exit', (code) => {
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(code)
+      }
+    })
+  })
+}
+
+const main = async () => {
+  console.log("===Preparing for build===")
+  await execCmd('./scripts/prep.sh')
+  
+  const template = core.getInput('template')
+  console.log(`===Building template ${template}===`)
+  await execCmd(`./scripts/build.sh ${template}`)
+
+  const time = (new Date()).toTimeString()
+  core.setOutput("time", time)
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+}
+
+main()
+  .catch((error) => {
+    core.setFailed(error.message)
+  })
